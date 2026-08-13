82a1566a9d5c7d7ef8b9389f08ef23270d83567e
mineda <mineda@gmail.com>
2026-08-13 19:33:44 -0300

    first commit

---
+++ src/main/java/br/gov/sp/cps/springlab420262/Springlab420262Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.cps.springlab420262;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlab420262Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlab420262Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/cps/springlab420262/Springlab420262ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.cps.springlab420262;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlab420262ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
